3fb2fcca67fc1e8531f9221f6b53a9b3ee02583d
Yuri Drabik <yurist38@mail.ru>
2020-10-27 14:53:43 +0100

    fix: add ignored errors for Sentry

--- src/background/index.ts
+++ src/background/index.ts
@@ -5,6 +5,7 @@ if (process.env.SENTRY_DSN) {
   init({
     dsn: process.env.SENTRY_DSN,
     release: `linkedin-video-downloader-ext@${process.env.npm_package_version}`,
+    ignoreErrors: [],
   });
   configureScope((scope) => {
     scope.setTag('app', 'background');
